a86ac5639a2f9f262f960dd37f591d15caa7757f
straiforos <stephen@traiforos.com>
2025-01-11 08:55:47 -0500

    TODO to make change to support concrete permissions.

--- bundles/org.openhab.core/src/main/java/org/openhab/core/auth/Permission.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/auth/Permission.java
@@ -1,8 +1,22 @@
+/**
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.core.auth;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.common.registry.Identifiable;
 
+// TODO implement something similar to opensmarthome to have concrete default permissions for openhab.
+// https://github.com/opensmarthouse/opensmarthouse-core/commit/ee3494ce183e379aaa3bdbb00da9496346518670#diff-2ae64de6c0f006865faef94035ee756ceb249a675f8848be9d63ea4ea08d4d2fR20
 /**
  * Defines a given access to a specific functionality.
  * <ul>
--- bundles/org.openhab.core/src/main/java/org/openhab/core/internal/auth/RoleRegistryImpl.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/internal/auth/RoleRegistryImpl.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.core.internal.auth;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -7,6 +19,11 @@ import org.openhab.core.auth.RoleRegistry;
 import org.openhab.core.common.registry.AbstractRegistry;
 import org.osgi.service.component.annotations.Component;
 
+
+/**
+ * Registry to persist user roles that are configured.
+ * @author Stephen Traiforos  (Triforce) - Initial contribution
+ */
 @NonNullByDefault
 @Component(service = RoleRegistry.class, immediate = true)
 public class RoleRegistryImpl extends AbstractRegistry<Role, String, RoleProvider> implements RoleRegistry {
