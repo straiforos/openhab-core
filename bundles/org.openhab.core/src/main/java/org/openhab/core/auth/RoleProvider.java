1af8d8ea14d024c3ce8e1736b48df789e3c4b571
straiforos <stephen@traiforos.com>
2025-01-11 09:54:04 -0500

    added headers

--- bundles/org.openhab.core/src/main/java/org/openhab/core/auth/RoleProvider.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/auth/RoleProvider.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.core.auth;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -6,7 +18,7 @@ import org.openhab.core.common.registry.Provider;
 /**
  * Role provider
  * 
- * @author Stephen Traiforos - Initial contribution
+ * @author Stephen Traiforos (Triforce) - Initial contribution
  */
 @NonNullByDefault
 public interface RoleProvider extends Provider<Role> {
